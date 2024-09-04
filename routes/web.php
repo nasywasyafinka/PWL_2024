e0ab8d247f754f0f085516795e917d04c11b935e
Nasywa Syafinka Widyamara <syafinkaforcolle@gmail.com>
2024-09-04 09:42:57 +0700

    pertemuan 2- praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,42 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'Nama : Nasywa Syafinka Widyamara | NIM: 2241760002';
+});
+
+Route::get('/user/{name}', function ($name){
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postID, $commentID){
+    return 'Pos ke-'.$postID." Komentar ke-".$commentID;
+});
+
+Route::get('/articles/{id}', function ($ID){
+    return 'Halaman artikel dengan ID '.$ID;
+});
+
+Route::get('/user/{name?}', function ($name='nasywa syafinka'){
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/{name?}', function ($name='John'){
+    return 'Nama saya '.$name;
 });
